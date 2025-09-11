d9b75624de1487789ff5ea0961b20a731eab5286
lionblaze55 <joshfaber.g@gmail.com>
2025-09-11 14:57:30 -0400

    Delete modules/flatpakclear directory

--- modules/flatpakclear/flatpakclear.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Uninstalling all Flatpak applications..."
-
-# Uninstall all user-installed Flatpaks
-flatpak uninstall -y --delete-data
-
-echo "Flatpak cleanup complete."
